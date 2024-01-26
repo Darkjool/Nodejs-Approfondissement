1110b213d5b4456d66e6979e6bbe64e8d79413b6
Darkjool <129541829+Darkjool@users.noreply.github.com>
2024-01-26 13:31:10 +0100

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,17 @@ module.exports = {
     {
       name: "app",
       script: "./www/app.js",
+      // Lancement de 3 tâches en parralèle
+      instances: 3,
+      // Mode cluster
+      exec_mode: "cluster", 
+      // Si erreur => Fichier .log
+      error_file: "./logs/err.log", 
       env_production: {
         NODE_ENV: "production",
       },
+      // Limite RAM = 200mo
+      max_memory_restart: "200M", 
     },
   ],
 };
